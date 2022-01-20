fb81a86c3c0d3f5f221927413d17b46c862a6ad7
Enrique Serrano <eserranodev@gmail.com>
2022-01-20 16:32:04 +0100

    Mappings improvements

--- .config/nvim/lua/mappings/init.lua
+++ .config/nvim/lua/mappings/init.lua
@@ -1,10 +1,22 @@
 -- Jump to previous file
 vim.api.nvim_set_keymap("n", "<TAB><TAB>", "<c-^>", {})
 
+-- Save file
+vim.cmd[[nnoremap <leader>s :w<cr>]]
+vim.cmd[[inoremap <leader>s <C-c>:w<cr>]]
+
+-- Quit editor
+vim.cmd[[noremap <leader>q :q<cr>]]
+
 -- Window movement
 vim.api.nvim_set_keymap("n", "gn", ":bnext<CR>", {})
 vim.api.nvim_set_keymap("n", "gb", ":bprevious<CR>", {})
 
+-- Panes movement
+vim.cmd[[noremap <leader>wl <C-w>l]]
+vim.cmd[[noremap <leader>wh <C-w>h]]
+vim.cmd[[noremap <leader>wj <C-w>j]]
+vim.cmd[[noremap <leader>wk <C-w>k]]
 
 -- Disable arrow keys
 vim.cmd[[nnoremap <up> <nop>]]
